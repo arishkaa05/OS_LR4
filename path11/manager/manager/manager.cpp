7ff15948f6054229c1f7230e043f0bc16e59a2e1
Арина Показацкая <pokazatskaya02@mail.ru>
2022-12-28 19:05:59 +0300

    parth 1

---
+++ path11/manager/manager/manager.cpp
@@ -0,0 +1,122 @@
+﻿
+#include <Windows.h>
+
+#define PAGES 11
+
+int main()
+{
+	DWORD pageSize;
+	HANDLE file, hMapping;
+	HANDLE readerSemaphore[PAGES];
+	HANDLE writerSemaphore[PAGES];
+	HANDLE processes[PAGES * 2];
+	PROCESS_INFORMATION writerInfo[PAGES];
+	PROCESS_INFORMATION readerInfo[PAGES];
+	SYSTEM_INFO siSysInfo;
+	STARTUPINFO startInfo;
+	SECURITY_ATTRIBUTES semaphoreSecurityAttributes = { sizeof(semaphoreSecurityAttributes), NULL, TRUE };
+	wchar_t buffer[100] = L"";
+
+	GetSystemInfo(&siSysInfo);
+	pageSize = siSysInfo.dwPageSize;
+
+	file = CreateFile(
+		L"D:\\OS_LR4\\path 1\\file.txt",
+		GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ | FILE_SHARE_WRITE,
+		NULL,
+		OPEN_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+	);
+
+	hMapping = CreateFileMapping(
+		file,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		pageSize * PAGES,
+		L"Local\\file");
+
+	for (int i = 0; i < PAGES; i++)
+	{
+		wchar_t writerName[] = L"writer ";
+		writerName[6] = '0' + i;
+		writerSemaphore[i] = CreateSemaphore(
+			&semaphoreSecurityAttributes,
+			1,
+			1,
+			writerName
+		);
+
+		wchar_t readerName[] = L"reader ";
+		readerName[6] = '0' + i;
+		readerSemaphore[i] = CreateSemaphore(
+			&semaphoreSecurityAttributes,
+			0,
+			1,
+			readerName
+		);
+	}
+	//информация для запуска нового процесса
+	ZeroMemory(&startInfo, sizeof(startInfo));
+	startInfo.cb = sizeof(startInfo);
+
+	for (int i = 0; i < PAGES; i++)
+	{
+		ZeroMemory(&readerInfo[i], sizeof(readerInfo[i]));
+		ZeroMemory(&writerInfo[i], sizeof(writerInfo[i]));
+
+		CreateProcess(
+			L"D:\\OS_LR4\\path 1\\reader\\x64\\Debug\\reader.exe",		// имя исполняемого файл
+			buffer,														// командная строка
+			NULL,														// атрибуты защиты процесса
+			NULL,														// атрибуты защиты потоков
+			TRUE,														// наследование дескрипторов
+			CREATE_SUSPENDED,											//поток нового процесса создается в приостановленном состоянии
+			NULL,														// блок памяти с переменными окружения
+			NULL,														// текущий диск и каталог для процесса
+			&startInfo,													// указывает на структуру STARTUPINFO
+			&readerInfo[i]												// инициализируемая структура
+		);
+		CreateProcess(
+			L"D:\\OS_LR4\\path 1\\writer\\x64\\Debug\\writer.exe",		// имя исполняемого файл
+			buffer,														// командная строка
+			NULL,														// атрибуты защиты процесса
+			NULL,														// атрибуты защиты потоков
+			TRUE,														// наследование дескрипторов
+			CREATE_SUSPENDED,											//поток нового процесса создается в приостановленном состоянии
+			NULL,														// блок памяти с переменными окружения
+			NULL,														// текущий диск и каталог для процесса
+			&startInfo,													// указывает на структуру STARTUPINFO
+			&readerInfo[i]												// инициализируемая структура
+		);
+
+		processes[i] = readerInfo[i].hProcess;
+		processes[PAGES * 2 - 1 - i] = writerInfo[i].hProcess;
+	}
+
+
+	for (int i = 0; i < PAGES; i++)
+		ResumeThread(writerInfo[i].hThread);
+	for (int i = 0; i < PAGES; i++)
+		ResumeThread(readerInfo[i].hThread);
+
+	WaitForMultipleObjects(PAGES * 2, processes, TRUE, INFINITE);		// ждем окончание процессов
+
+	for (int i = 0; i < PAGES; i++)
+	{
+		CloseHandle(writerInfo[i].hProcess);
+		CloseHandle(writerInfo[i].hThread);
+
+		CloseHandle(readerInfo[i].hProcess);
+		CloseHandle(readerInfo[i].hThread);
+
+		CloseHandle(readerSemaphore[i]);
+		CloseHandle(writerSemaphore[i]);
+	}
+	CloseHandle(hMapping);
+	CloseHandle(file);
+
+	return 0;
+}
---
+++ path11/reader/reader/reader.cpp
@@ -0,0 +1,96 @@
+﻿
+#include <Windows.h>
+#include <fstream>
+#include <time.h>
+#include <string>
+
+#define PAGES 11
+
+using namespace std;
+
+int main()
+{
+	DWORD processId, start, pageSize;
+	HANDLE hFileMappingObject;
+	HANDLE readerSemaphore[PAGES];
+	HANDLE writerSemaphore[PAGES];
+	SYSTEM_INFO siSysInfo;
+	fstream log;
+	string logName;
+	void* viewOfFile;
+
+	GetSystemInfo(&siSysInfo);
+	pageSize = siSysInfo.dwPageSize;
+
+	processId = GetCurrentProcessId();				//извлекает идентификатор процесса вызывающего процесса
+	start = GetTickCount64();
+
+	srand(processId);								//используется для установки начала последовательности
+
+	logName = "D:\\OS_LR4\\path1\\logs\\reader_log_" + to_string(processId) + ".txt";
+	log.open(logName.c_str(), fstream::app);
+
+
+	hFileMappingObject = OpenFileMapping(			//открывает объект "проецируемый файл"
+		FILE_MAP_READ,								// режим доступа
+		FALSE,										// наследование
+		L"Local\\file"								// имя объекта
+	);
+
+	viewOfFile = MapViewOfFile(						//отображает представление проецируемого файла в адресное пространство вызывающего процесса
+		hFileMappingObject,							// дескр. объекта проецируемый файл
+		FILE_MAP_READ,								// режим доступа
+		0, 0,										// смещение
+		PAGES * pageSize);							// число отображаемых байтов
+
+	VirtualLock(viewOfFile, PAGES * pageSize);		// Блокирует указанную область ВАП процесса в физической памяти
+
+	for (int i = 0; i < PAGES; i++)
+	{
+		wchar_t writerName[] = L"writer ";
+		writerName[6] = '0' + i;
+		writerSemaphore[i] = OpenSemaphore(			//oткрывает существующий именованный объект семафора
+			SYNCHRONIZE | SEMAPHORE_MODIFY_STATE,	//доступ
+			FALSE,									//процессы не наследуют дескриптор
+			writerName);							//имя открываемого семафора.
+
+		wchar_t readerName[] = L"reader ";
+		readerName[6] = '0' + i;
+		readerSemaphore[i] = OpenSemaphore(			//oткрывает существующий именованный объект семафора
+			SYNCHRONIZE | SEMAPHORE_MODIFY_STATE,	//доступ
+			FALSE,									//процессы не наследуют дескриптор
+			readerName);							//имя открываемого семафора.
+	}
+	// Чтение
+	for (int i = 0; i < PAGES; i++)
+	{
+		int page = WaitForMultipleObjects(			//ждет, когда массив объектов перейдет в сигнальное состояние
+			PAGES,									// количество объектов в массиве
+			readerSemaphore,						// массив объектов
+			FALSE,									// ждем любого объекта
+			INFINITE);
+
+		log << processId << "\t" << page << "\t" << "READ_START" << "\t" << GetTickCount64() - start << "\t" << GetTickCount64() << "\t" << start << "\n";
+		char* buf = (char*)((char*)viewOfFile + page * pageSize);
+		char buffer[2];
+		buffer[0] = buf[0];
+		buffer[1] = buf[1];
+		Sleep(500 + rand() % 1000);
+		log << processId << "\t" << page << "\t" << "READ_END" << "\t" << GetTickCount64() - start << "\t" << GetTickCount64() << "\t" << start << "\n";
+
+		ReleaseSemaphore(							//увеличивает значение счетчика семафора
+			writerSemaphore[page],					//идентификатор семафора 
+			1,										//значение инкремента 
+			NULL);									//адрес переменной для записи 
+		log << processId << "\t" << page << "\t" << "WSEM_RELEASED" << "\t" << GetTickCount64() - start << "\t" << GetTickCount64() << "\t" << start << "\n";
+	}
+
+	for (int i = 0; i < PAGES; i++)					// Закрытие дескрипторов
+	{
+		CloseHandle(readerSemaphore[i]);
+		CloseHandle(writerSemaphore[i]);
+	}
+	VirtualUnlock(viewOfFile, pageSize);			//разблокирует указанный диапазон страниц в ВАП процесса
+	UnmapViewOfFile(viewOfFile);					//отменяет сопоставление представления файла из адресного пространства вызывающего процесса
+	log.close();
+}
