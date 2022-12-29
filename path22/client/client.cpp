3f1aed37cf1fcdfa622fd7b27709cefe74282594
arishkaa05 <81327095+arishkaa05@users.noreply.github.com>
2022-12-29 13:10:44 +0300

    Add files via upload
    ошибка

---
+++ path22/client/client.cpp
@@ -0,0 +1,40 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void CALLBACK lpCompletionRoutine(DWORD errorCode, DWORD bytestransfered, LPOVERLAPPED lpOverlapped)
+{
+}
+
+int main()
+{
+    HANDLE hNamedPipe;
+    OVERLAPPED lpOverlapped;
+    LPCWSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";
+
+    char* c = new char[MAX_PATH];
+
+    ZeroMemory(&lpOverlapped, sizeof(lpOverlapped));
+
+    hNamedPipe = CreateFile(
+        lpszPipeName,
+        GENERIC_READ | GENERIC_WRITE,
+        0, NULL, OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (hNamedPipe == INVALID_HANDLE_VALUE) {
+        cout << "Error " << GetLastError() << ".\n";
+        SetLastError(0);
+    }
+    cout << "Data from server\n";
+    while (true) {
+        ReadFileEx(hNamedPipe, c, 512, &lpOverlapped, lpCompletionRoutine);
+        cout << c << endl << endl;
+        SleepEx(INFINITE, TRUE);
+
+    }
+
+    return 0;
+}
---
+++ path22/server/server.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    HANDLE hNamedPipe;
+    OVERLAPPED lpOverlapped;
+    LPCWSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";
+    DWORD written;
+    char* c = new char[MAX_PATH];
+
+    ZeroMemory(&lpOverlapped, sizeof(lpOverlapped));
+    lpOverlapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+    hNamedPipe = CreateNamedPipe(
+        lpszPipeName,
+        PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED, //Для чтения и записи
+        PIPE_TYPE_BYTE | PIPE_WAIT, //ориентирован на передачу байт Канал будет работать в блокирующем режиме, когда процесс переводится в состояние ожидания до завершения операций в канале
+        1,                          // максимальное количество реализаций канала 
+        512, 512,                   // размер выходного/входного  буфера в байтах 
+        0,                          // время ожидания в миллисекундах 
+        NULL);                      // адрес переменной для атрибутов защиты
+
+    ConnectNamedPipe(hNamedPipe, &lpOverlapped);
+
+    cout << "Enter data '\n' Enter \"END\" to end the program\n";
+    while (true) {
+        cin >> c;
+        if (strcmp(c, "END") != 0)
+        {
+            cout << endl;
+            WriteFileEx(hNamedPipe,
+                c, strlen(c) + 1,
+                &written,
+                &lpOverlapped, );
+            WaitForSingleObject(lpOverlapped.hEvent, INFINITE);
+        }
+        else
+        {
+            cout << "\nServer finished\n";
+            break;
+        }
+    }
+}
+
+
